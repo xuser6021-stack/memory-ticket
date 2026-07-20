27dfbf8a5cec655b77ca9df16f1bfe2039b15b02
Sanjay <sanjay.24.0530@gmail.com>
2026-07-20 23:06:27 +0530

    feat: setup shadcn ui and project architecture

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Memory Ticket</h1>
-      <p>My first full-stack project 🚀</p>
+    <main className="min-h-screen flex items-center justify-center p-8">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle>Memory Ticket</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <p>My first full-stack project 🚀</p>
+        </CardContent>
+      </Card>
     </main>
   );
 }
